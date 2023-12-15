54cb69a4ae5243157f7ee971512955ef9baeb9a0
CatWithCode <56090976+CatWithCode@users.noreply.github.com>
2023-12-15 08:19:10 +0100

    Test

--- Assets/MainFunctions.js
+++ Assets/MainFunctions.js
@@ -1,28 +1,43 @@
+// Public Functions:
+
+// Reading and Writing Page-Header:
 function WriteHeader() {
-    document.write('<div class="Header">\
-            <h2 class="Header-Border" align="center">\
-                <a href="/"> <img src="/Assets/favicon.png" alt="Cute Pixelart Catgirl" style="width:55px;height:55px";></a>\
-                <a href="/">CatWithCode</a>\
-                <br>\
-                <br>\
-                <b>&nbsp;</b>\
-                <a href="/blog.html">BLOG</a>\
-                <b>&nbsp;</b>\
-                <a href="/privacy.html">PRIVACY</a>\
-                <b>&nbsp;</b>\
-                <a href="/license.html">LICENSE</a>\
-                <b>&nbsp;</b>\
-                <a href="/contact.html">CONTACT</a>\
-                <b>&nbsp;</b>\
-                <a href="https://catwithcode.moe/Feed/RSS.xml">📡&nbsp;RSS</a>\
-                <b>&nbsp;</b>\
-            </h2>\
-        </div>\
-    ');
+    var test = loadFile('/Assets/BaseFiles/TopBar.html')
+
+    if(test != null){
+        document.write(loadFile('/Assets/BaseFiles/TopBar.html'));
+    } else {
+        document.write('<div class="Header">\
+                <h2 class="Header-Border" align="center">\
+                    <a href="/"> <img src="/Assets/favicon.png" alt="Cute Pixelart Catgirl" style="width:55px;height:55px";></a>\
+                    <a href="/">CatWithCode</a>\
+                    <br>\
+                    <br>\
+                    <b>&nbsp;</b>\
+                    <a href="/blog.html">BLOG</a>\
+                    <b>&nbsp;</b>\
+                    <a href="/privacy.html">PRIVACY</a>\
+                    <b>&nbsp;</b>\
+                    <a href="/license.html">LICENSE</a>\
+                    <b>&nbsp;</b>\
+                    <a href="/contact.html">CONTACT</a>\
+                    <b>&nbsp;</b>\
+                    <a href="https://catwithcode.moe/Feed/RSS.xml">📡&nbsp;RSS</a>\
+                    <b>&nbsp;</b>\
+                </h2>\
+            </div>\
+        ');
+    }
 }
 
+// Reading and Writing Footer-Header:
 function WriteFooter(dateText) {
-    var output = '<h5 class="Fooder">'
+    var test = loadFile('/Assets/BaseFiles/TopBar.html')
+
+    if(test != null){
+        document.write(loadFile('/Assets/BaseFiles/Footer.html').replace("###DATE_TEXT###", dateText));
+    } else {
+        var output = '<h5 class="Fooder">'
         .concat(dateText) // Date
             .concat('&nbsp;&nbsp;|&nbsp;&nbsp;')
                 .concat('©️ CatWithCode') // Maker
@@ -30,5 +45,24 @@ function WriteFooter(dateText) {
                         .concat('CC BY-NC-ND 4.0') // License
                             .concat('</h5>');
                             
-    document.write(output);
+        document.write(output);
+    }
 }
+
+
+// Internel Functions:
+function loadFile(filePath) {
+    // Creating Request:
+    var xmlhttp = new XMLHttpRequest();
+
+    // Getting the File: 
+    xmlhttp.open("GET", filePath, false);
+    xmlhttp.send();
+
+    // Retrun if the File was found, else do fallback:
+    if (xmlhttp.status==200) {
+        return xmlhttp.responseText;
+    } else {
+        return null;
+    }
+  }
