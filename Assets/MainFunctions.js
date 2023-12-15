ea46ae3914101730fef4f4f517ff26707a98d03c
CatWithCode <56090976+CatWithCode@users.noreply.github.com>
2023-12-15 10:43:15 +0100

    Quick Cleanup

--- Assets/MainFunctions.js
+++ Assets/MainFunctions.js
@@ -10,26 +10,10 @@ async function WriteFooter(dateText) {
     document.getElementById("Footer").innerHTML = (await this.aSyncLoadFile('/Assets/BaseFiles/Footer.html')).replace("###DATE_TEXT###", dateText);
 }
 
-
 // Internel Functions:
 // Loads File from a desired location asynchronously:
 async function aSyncLoadFile(filePath) {
     return fetch(filePath)
         .then((response)=>response.text())
         .then((responseText)=>{return responseText});
-}
-
-// Loads File from a desired location synchronised:
-function syncLoadFile(filePath) {
-    // Creating Request:
-    var xmlhttp = new XMLHttpRequest();
-
-    // Getting the File: 
-    xmlhttp.open("GET", filePath, false);
-    xmlhttp.send();
-
-    // Retrun if the File was found, else do fallback:
-    if (xmlhttp.status==200) {
-        return xmlhttp.responseText;
-    }
 }
